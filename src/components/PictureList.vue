970e9fa5fdf88e884572eb66184c7af59cd68742
zyb <cmefh@qq.com>
2025-02-01 01:16:36 +0800

    ESplatform-frontend

---
+++ src/components/PictureList.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-list
+    item-layout="horizontal"
+    :grid="{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 4, xxl: 3 }"
+    :data-source="props.pictureList"
+  >
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a-card hoverable>
+          <template #cover>
+            <img :src="item.url" />
+          </template>
+          <a-card-meta>
+            <template #title>
+              <a-tooltip :title="item.title">
+                <span class="ellipsis-text">{{ item.title }}</span>
+              </a-tooltip>
+            </template>
+          </a-card-meta>
+        </a-card>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import { withDefaults, defineProps } from "vue";
+
+interface Props {
+  pictureList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  pictureList: () => [],
+});
+</script>
+
+<style scoped>
+.ellipsis-text {
+  display: inline-block;
+  max-width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
